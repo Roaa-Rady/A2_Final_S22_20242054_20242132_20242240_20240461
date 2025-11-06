ed6beabedcde5facf05da32afea9624386d1db3e
Aamen <aamenmoh2006@gmail.com>
2025-11-06 20:45:34 +0200

    Update MainComponent.cpp

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,22 +1,27 @@
 #include "MainComponent.h"
-
+#include "PlayerGUI.h"
 MainComponent::MainComponent()
 {
-    player1 = std::make_unique<PlayerGUI>();
+    player1 = std::make_unique<PlayerGUI>("Player1");
     addAndMakeVisible(player1.get());
     mixer.addInputSource(&player1->playerAudio, false);
-    player2 = std::make_unique<PlayerGUI>();
-    mixer.addInputSource(&player2->playerAudio, false);
-     addAndMakeVisible(player2.get());
 
-    setSize(500,400);
+    player2 = std::make_unique<PlayerGUI>("Player2");
+    mixer.addInputSource(&player2->playerAudio, false);
+    addAndMakeVisible(player2.get());
+
+    setSize(500, 400);
     setAudioChannels(0, 2);
 }
 
+
 MainComponent::~MainComponent()
 {
+    if (player1) player1->saveLastSession();
+    if (player2) player2->saveLastSession();
     shutdownAudio();
 }
+
 
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
@@ -43,5 +48,6 @@ void MainComponent::resized()
     player1->setBounds(0, 0, getWidth(), getHeight() / 2);
     player2->setBounds(0, getHeight() / 2, getWidth(), getHeight() / 2);
 }
+
 
 
